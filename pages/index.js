2b81e43baf9fd9c843e4a1d1d8670e02e7c74ab6
Guillaume Dormoy <g.dormoy@outlook.com>
2020-08-01 17:11:00 +0200

    :sparkles: netlify identity

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,41 @@
+import { useEffect, useState } from 'react'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
+import netlifyAuth from './netlifyAuth.js'
+
 
 export default function Home() {
+    let [user, setUser] = useState(null)
+    let [loggedIn, setLoggedIn] = useState(netlifyAuth.isAuthenticated)
+
+    useEffect(() => {
+        let isCurrent = true
+        netlifyAuth.initialize((user) => {
+            if (isCurrent) {
+                setLoggedIn(!!user)
+            }
+        })
+
+        return () => {
+            isCurrent = false
+        }
+    })
+
+    let login = () => {
+        netlifyAuth.authenticate((user) => {
+            setLoggedIn(!!user)
+            setUser(user)
+            netlifyAuth.closeModal()
+        })
+    }
+
+    let logout = () => {
+        netlifyAuth.signout(() => {
+            setLoggedIn(false)
+            setUser(null)
+        })
+    }
+
   return (
     <div className={styles.container}>
       <Head>
@@ -10,55 +44,23 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+          {loggedIn ? (
+              <div>
+                  You are logged in!
+                  {user && <>Welcome {user?.user_metadata.full_name}!</>}
 
-        <p className={styles.description}>
-          Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+                  <button onClick={logout}>
+                      Log-out here.
+                  </button>
+              </div>
+          ) : (
+              <button onClick={login}>
+                  Log in here.
+              </button>
+          )}
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
-        </a>
       </footer>
     </div>
   )
